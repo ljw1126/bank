19d908289962c39ef40b9b672c40cb392e5932e3
leejinwoo <leejinwoo1126@gmail.com>
2023-08-22 15:11:34 +0900

    test : JoinRequestDto invalid case test

--- src/test/java/shop/mtcoding/bank/web/UserControllerTest.java
+++ src/test/java/shop/mtcoding/bank/web/UserControllerTest.java
@@ -18,6 +18,8 @@ import shop.mtcoding.bank.domain.user.UserRepository;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static shop.mtcoding.bank.dto.user.UserRequestDto.JoinRequestDto;
 
 @ActiveProfiles("test")
@@ -48,11 +50,7 @@ class UserControllerTest extends DummyObject {
     @Test
     void join_test() throws Exception {
         //given
-        JoinRequestDto joinRequestDto = new JoinRequestDto();
-        joinRequestDto.setUsername("love");
-        joinRequestDto.setPassword("1234");
-        joinRequestDto.setEmail("love@gmail.com");
-        joinRequestDto.setFullname("러브");
+        JoinRequestDto joinRequestDto = getJoinRequestDto("love", "1234", "love@gmail.com", "러브");
 
 
         //when
@@ -63,20 +61,16 @@ class UserControllerTest extends DummyObject {
 
         //then
         resultActions.andDo(print())
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("회원가입 성공"));
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.code").value(1))
+                .andExpect(jsonPath("$.msg").value("회원가입 성공"));
     }
 
     @DisplayName("중복아이디가 있을 경우 bad request를 반환한다")
     @Test
     void join_test_when_exist_duplicate_id() throws Exception {
         //given
-        JoinRequestDto joinRequestDto = new JoinRequestDto();
-        joinRequestDto.setUsername("aaa");
-        joinRequestDto.setPassword("1234");
-        joinRequestDto.setEmail("aaa@gmail.com");
-        joinRequestDto.setFullname("에이");
+        JoinRequestDto joinRequestDto = getJoinRequestDto("aaa", "1234", "aaa@gmail.com", "이름없음");
 
         //when
         ResultActions resultActions = mockMvc.perform(post("/api/join")
@@ -86,9 +80,91 @@ class UserControllerTest extends DummyObject {
 
         //then
         resultActions.andDo(print())
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("-1"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("동일한 username이 존재합니다"));
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("-1"))
+                .andExpect(jsonPath("$.msg").value("동일한 username이 존재합니다"));
     }
+
+    @DisplayName("username에 한글은 안된다")
+    @Test
+    void join_by_invalid_username() throws Exception {
+        //given
+        JoinRequestDto joinRequestDto = getJoinRequestDto("한글이름안됨", "1234", "empty@gmail.com", "이름없음");
+
+        //when
+        //then
+        mockMvc.perform(post("/api/join")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(joinRequestDto))
+                ).andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("-1"))
+                .andExpect(jsonPath("$.msg").value("유효성 검사 실패"))
+                .andExpect(jsonPath("$.data.username").value("영문/숫자 2~20자 이내로 작성해주세요"));
+    }
+
+    @DisplayName("비밀번호는 4~20 사이의 길이를 가진다")
+    @Test
+    void join_by_invalid_password() throws Exception {
+        //given
+        JoinRequestDto joinRequestDto = getJoinRequestDto("empty", "1", "empty@gmail.com", "이름없음");
+
+        //when
+        //then
+        mockMvc.perform(post("/api/join")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(joinRequestDto))
+                ).andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("-1"))
+                .andExpect(jsonPath("$.msg").value("유효성 검사 실패"))
+                .andExpect(jsonPath("$.data.password").value("size must be between 4 and 20"));
+    }
+
+    @DisplayName("유효하지 이메일 형식으로 email 입력 해야 한다")
+    @Test
+    void join_by_invalid_email() throws Exception {
+        //given
+        JoinRequestDto joinRequestDto = getJoinRequestDto("empty", "11234", "invalid@email.co.kr", "이름없음");
+
+        //when
+        //then
+        mockMvc.perform(post("/api/join")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(joinRequestDto))
+                ).andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("-1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("유효성 검사 실패"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.email").value("이메일 형식으로 작성해주세요"));
+    }
+
+    @DisplayName("fullname은 한글/영문 포함 2~20자로 작성해야 한다")
+    @Test
+    void join_by_invalid_fullname() throws Exception {
+        //given
+        JoinRequestDto joinRequestDto = getJoinRequestDto("empty", "11234", "valid@email.com", "힣");
+
+        //when
+        //then
+        mockMvc.perform(post("/api/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(joinRequestDto))
+                ).andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("-1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("유효성 검사 실패"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.fullname").value("한글/영문 2~20자 이내로 작성해 주세요"));
+    }
+
+    private static JoinRequestDto getJoinRequestDto(String username, String password, String email, String fullname) {
+        JoinRequestDto joinRequestDto = new JoinRequestDto();
+        joinRequestDto.setUsername(username);
+        joinRequestDto.setPassword(password);
+        joinRequestDto.setEmail(email);
+        joinRequestDto.setFullname(fullname);
+        return joinRequestDto;
+    }
+
 
 }
