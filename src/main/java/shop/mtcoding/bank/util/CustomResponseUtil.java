029fbf2fb288fbac95a81cd96e0cddbbcfb9b04b
leejinwoo <leejinwoo1126@gmail.com>
2023-08-23 22:17:34 +0900

    feat : common succes response method

--- src/main/java/shop/mtcoding/bank/util/CustomResponseUtil.java
+++ src/main/java/shop/mtcoding/bank/util/CustomResponseUtil.java
@@ -12,6 +12,20 @@ import java.io.IOException;
 public class CustomResponseUtil {
     private static final Logger log = LoggerFactory.getLogger(CustomResponseUtil.class);
 
+    public static void success(HttpServletResponse response, Object dto) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            ResponseDto<?> responseDto = new ResponseDto<>(1, "로그인 성공", dto);
+            String responseBody = objectMapper.writeValueAsString(responseDto);
+
+            response.setContentType("application/json; charset=utf-8");
+            response.setStatus(200);
+            response.getWriter().println(responseBody);
+        } catch(Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
     // 인증 실패
     public static void unAuthentication(HttpServletResponse response, String msg) {
         try {
