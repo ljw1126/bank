9f3a20e4b698bdeb8b3e3d0eef3139f9be99a54c
leejinwoo <leejinwoo1126@gmail.com>
2023-08-23 22:18:56 +0900

    feat : JwtAuthenticationFilter 구현

---
+++ src/main/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,70 @@
+package shop.mtcoding.bank.config.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import shop.mtcoding.bank.config.auth.LoginUser;
+import shop.mtcoding.bank.dto.user.UserRequestDto;
+import shop.mtcoding.bank.dto.user.UserResponseDto;
+import shop.mtcoding.bank.dto.user.UserResponseDto.LoginResponseDto;
+import shop.mtcoding.bank.util.CustomResponseUtil;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static shop.mtcoding.bank.dto.user.UserRequestDto.*;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+        setFilterProcessesUrl("/api/login");
+        this.authenticationManager = authenticationManager;
+    }
+
+    // Post : /login 시 강제 로그인 진행
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        try {
+            ObjectMapper om = new ObjectMapper();
+            LoginRequestDto loginRequestDto = om.readValue(request.getInputStream(), LoginRequestDto.class);
+
+            // 강제 로그인
+            UsernamePasswordAuthenticationToken authenticationToken
+                    = new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(), loginRequestDto.getPassword());
+
+            // UserDeatilService의 loadByUsername 호출
+            // JWT 를 쓴다 하더라도, 컨트로럴 진입 하면 시큐리티의 권한 체크, 인증 체크의 도움을 받을 수 있게 세션을 만듦
+            // 이 세션의 유효기간은 request에 대한 response 후 제거됨 (매번 request 요청시 생성, stateless)
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            return authentication;
+        } catch (Exception e) {
+            // authenciationEntryPoint 걸린다. (SecurityConfig)
+            throw new InternalAuthenticationServiceException(e.getMessage());
+        }
+    }
+
+    // return authentication 잘 작동하면 successfulAuthentication 메서드 호출됨
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        LoginUser loginUser = (LoginUser) authResult.getPrincipal();
+        String jwtToken = JwtProcess.create(loginUser);
+        response.addHeader(JwtVO.HEADER, jwtToken);
+
+        LoginResponseDto loginResponseDto = new LoginResponseDto(loginUser.getUser());
+        CustomResponseUtil.success(response, loginResponseDto);
+    }
+}
+
+
