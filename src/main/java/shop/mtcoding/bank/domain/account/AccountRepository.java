38085e90f646275669811d2c1fc89553a4ac03a9
leejinwoo <leejinwoo1126@gmail.com>
2023-08-25 16:35:33 +0900

    docs : Lazy Loading 관련

--- src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
+++ src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
@@ -1,6 +1,8 @@
 package shop.mtcoding.bank.domain.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,8 +11,9 @@ import java.util.Optional;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    // TODO. 리팩토링, 계좌/소유자 확인시에 쿼리가 두번 나가기 때문에
-    Optional<Account> findByNumber(Long number);
+    //join fetch를 하면 조인해서 객체에 값을 미리 가져올 수 있다
+    //@Query("SELECT ac FROM Account ac JOIN FETCH ac.user u where ac.number = :number")
+    Optional<Account> findByNumber(@Param("number") Long number);
 
     // jpa query method
     // select * from account where user_id = :id;
