1ff8d994f94461e20e4b20e68076b9eb46d28dd6
leejinwoo <leejinwoo1126@gmail.com>
2023-08-25 21:26:46 +0900

    feat : DummyObject 데이터 추가

--- src/main/java/shop/mtcoding/bank/config/dummy/DummyDevInit.java
+++ src/main/java/shop/mtcoding/bank/config/dummy/DummyDevInit.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.account.AccountRepository;
 import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserRepository;
 
@@ -12,10 +14,17 @@ public class DummyDevInit extends DummyObject{
 
     @Profile("dev") // dev profile에서만 무조건 실행
     @Bean
-    CommandLineRunner init(UserRepository userRepository) {
+    CommandLineRunner init(UserRepository userRepository, AccountRepository accountRepository) {
         return (args) -> {
-            User user = userRepository.save(newUser("aaaa", "1234"));
+            User ssar = userRepository.save(newUser("ssar", "쌀"));
+            User cos = userRepository.save(newUser("cos", "코스,"));
+            User love = userRepository.save(newUser("love", "러브"));
+            User admin = userRepository.save(newUser("admin", "관리자"));
 
+            Account ssarAccount1 = accountRepository.save(newMockAccount(1111L, ssar));
+            Account cosAccount = accountRepository.save(newMockAccount(2222L, cos));
+            Account loveAccount = accountRepository.save(newMockAccount(3333L, love));
+            Account ssarAccount2 = accountRepository.save(newMockAccount(4444L, ssar));
         };
     }
 }
