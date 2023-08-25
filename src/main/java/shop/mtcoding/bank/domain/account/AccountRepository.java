03cfae0546da5e55f00fa128226a89ebcf07066b
leejinwoo <leejinwoo1126@gmail.com>
2023-08-25 14:05:28 +0900

    feat(AccountService) : 신규 계좌 등록

--- src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
+++ src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
@@ -3,6 +3,11 @@ package shop.mtcoding.bank.domain.account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    // TODO. 리팩토링
+    Optional<Account> findByNumber(Long number);
 }
